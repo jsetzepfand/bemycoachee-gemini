e1123dfe12ccdf2b5e89dffe4b0b70bbcec47185
Jörg Setzepfand <jorg_setzepfand@singular.uk>
2025-09-28 11:21:28 +0200

    fix for AWS config

---
+++ src/aws-exports.d.ts
@@ -0,0 +1,3 @@
+// This declaration file tells TypeScript that the aws-exports.js module exists.
+// It prevents build errors when importing this auto-generated JavaScript file.
+declare module '../aws-exports';
