2bd985de8e71794fe2e2ab2598107c6e1f759da9
Jörg Setzepfand <jorg_setzepfand@singular.uk>
2025-10-10 20:17:56 +0200

    backend fix 7

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,33 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { factory as bemycoacheeApiFactory } from './api/resource';
+import { defineFunction, defineApi } from '@aws-amplify/backend-rest-api';
+import { Stack } from 'aws-cdk-lib';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
-// This is the main backend definition file.
-// We are passing a dictionary of resources directly to defineBackend.
-// The key 'bemycoacheeAPI' will be used to name the resource in the generated outputs.
-defineBackend({
-  bemycoacheeAPI: bemycoacheeApiFactory,
+// Create the Lambda function from the handler file
+const expressLambda = defineFunction({
+  entry: './api/handler.ts',
 });
+
+// Create the REST API and link it to the Lambda function
+const api = defineApi({
+  name: 'bemycoacheeAPI',
+  paths: {
+    '/messages': {
+      handler: expressLambda,
+    },
+  },
+});
+
+// Create the backend definition
+const backend = defineBackend({
+  api,
+});
+
+// Grant the Lambda function access to the DynamoDB table
+const cfnStack = Stack.of(backend.api.resources.stack);
+const policy = new PolicyStatement({
+  actions: ['dynamodb:Query', 'dynamodb:PutItem'],
+  resources: ['arn:aws:dynamodb:eu-central-1:315374878108:table/bemycoachee-chat-messages'],
+});
+
+backend.api.resources.functions.bemycoacheeAPI.addToRolePolicy(policy);
